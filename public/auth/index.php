fc8677bb89cda777a2d2b7672a82fcc27b905c9d
Shiv K Sah <shiv.sah@icreon.com>
2015-07-16 20:43:25 +0530

    gwstoken auth validation module - tenent-id validation

--- public/auth/index.php
+++ public/auth/index.php
@@ -5,31 +5,38 @@ $redis_ttl = 60*60*24;
 
 // error_log('\n\n$_SERVER => '.print_r($_SERVER,true)."==\n\n",3,"/var/www/html/auth/log.txt");
 // header("Auth-Status: OK");
-// header('HTTP/1.0 403 Forbidden');
-// header('HTTP/1.0 200 OK');
+// // header('HTTP/1.0 403 Forbidden');
+// // header('HTTP/1.0 200 OK');
 // echo "true";
 // exit;
 
 //Connecting to Redis server on localhost
 $redis = new Redis();
 $redis->connect('127.0.0.1', 6379);
-//echo "Connection to server sucessfully";
 
-
+//------- Get Client Information ---------------------//
 if(isset($_SERVER['HTTP_X_ORIGINAL_URI'])) {
-	$query_params = $_SERVER['HTTP_X_ORIGINAL_URI'];
-} else {
-	$query_params = $_SERVER['REQUEST_URI'];
+    $query_params = $_SERVER['HTTP_X_ORIGINAL_URI'];
 }
 $params = parse_url($query_params);
 parse_str($params['query'], $paramArr);
 
 $token = $paramArr['gwstoken'];
-error_log(date("Y-m-d H:i:s") . " - Request called from => $token \n",3,"/var/www/html/auth/log.txt");
+$tenant_id = 0;
+
+//------- Get Application Information ---------------------//
+$request_params = $_SERVER['REQUEST_URI'];
+$request_params = parse_url($request_params);
+parse_str($request_params['query'], $requestArr);
+$application_tenant_id = $requestArr['tenant_id']; 
+
+error_log(date("Y-m-d H:i:s") . " - Request called Application Tenant-ID = $application_tenant_id, User Tenant-ID = $tenant_id, gwstoken = $token \n",3,"/var/www/html/auth/log.txt");
+
 
 if (!empty($token)) {
-    $apiurl = "https://api.classlink.com/token/$token";
-    $header = array("token: $token");
+    //$apiurl = "https://api.classlink.com/token/$token";
+    $apiurl = "https://api.classlink.com/profile";
+    $header = array("gwstoken: $token");
     $ch = curl_init();
     $optArray = array(
         CURLOPT_URL => $apiurl,
@@ -41,15 +48,12 @@ if (!empty($token)) {
     if (!empty($output)) {
         curl_close($ch);
         $outputArr = json_decode($output, true);
-        if($outputArr['status']) {
-        	$redis->setex($token, $redis_ttl, 1);
-        	header('HTTP/1.0 200 OK');
-        	echo "OK";
-        	exit;
+        if($outputArr['status'] && $outputArr['response']['tenantid']==$application_tenant_id) {
+            $redis->setex($token, $redis_ttl, 1);
+            header('HTTP/1.0 200 OK');
+            echo "OK";
+            exit;
         }
-        //echo "<pre>";
-	    //print_r($outputArr);
-	    //exit;
     }
 }
 header('HTTP/1.0 403 Forbidden');
