ce27c8bbdcdf9a54faf6aa9f2b0b3daf9117980e
Shiv K Sah <shiv.sah@icreon.com>
2015-08-18 22:10:02 +0530

    installer : installation improvement

--- app/config/install/proxy.php
+++ app/config/install/proxy.php
@@ -5,5 +5,6 @@ return array(
     'listen_port'       => '{{listenPort}}', //'443',
     'config_path'       => '{{confPath}}', //'/var/www/html/classlinkproxy/configs',
     'nginx_service_path'=> '{{nginxPath}}', //'/var/www/html/php_root',
-    'auth_url'          => 'http://{{baseUrl}}/auth/'
+    'auth_url'          => (((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS']!=='off') || $_SERVER['SERVER_PORT']==443) ? 'https://':'http://' ).$_SERVER['HTTP_HOST'].'/auth/'
+//  'auth_url'          => 'http://localhost:9001/'
 );
