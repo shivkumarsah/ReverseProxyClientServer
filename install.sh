f15a2b86a61e4136add9ef4282fa7f54cd286460
Shiv K Sah <shiv.sah@icreon.com>
2015-09-01 12:50:13 +0530

    Nginx service controller setup

--- install.sh
+++ install.sh
@@ -5,3 +5,10 @@ chmod -Rf 0777 app/storage/logs
 chmod -Rf 0777 app/storage/logs/query-logs
 chmod -Rf 0777 public/install
 echo "Default permission updated successfully!";
+echo "***********Nginx service process start ***************";
+chown root nginx_root.sh
+chmod u=rwx,go=xr nginx_root.sh
+gcc nginx_root.c -o nginx_root
+chown root nginx_root
+chmod u=rwx,go=xr,+s nginx_root
+echo "***********Nginx service created successfully!!*******";
