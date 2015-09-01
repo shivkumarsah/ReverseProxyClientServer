86c0edc2a1289c3b80a4eb99eea5aa6aa46a6b8f
Shiv K Sah <shiv.sah@icreon.com>
2015-09-01 18:13:02 +0530

    installer : application list -> check for valid key

--- install.sh
+++ install.sh
@@ -5,10 +5,9 @@ chmod -Rf 0777 app/storage/logs
 chmod -Rf 0777 app/storage/logs/query-logs
 chmod -Rf 0777 public/install
 echo "Default permission updated successfully!";
-echo "***********Nginx service process start ***************";
-chown root nginx_root.sh
-chmod u=rwx,go=xr nginx_root.sh
-gcc nginx_root.c -o nginx_root
-chown root nginx_root
-chmod u=rwx,go=xr,+s nginx_root
-echo "***********Nginx service created successfully!!*******";
+chown root nginx/nginx_root.sh
+chmod u=rwx,go=xr nginx/nginx_root.sh
+gcc nginx/nginx_root.c -o nginx/nginx_root
+chown root nginx/nginx_root
+chmod u=rwx,go=xr,+s nginx/nginx_root
+echo "Nginx service created successfully!";
