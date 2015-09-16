c6c54c76b7c536d7c7dc417d7cef69860ceae070
Shiv K Sah <shiv.sah@icreon.com>
2015-09-16 14:13:59 +0530

    installation script added to configure nginx hosts

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 chmod -Rf 0777 configs
 chmod -Rf 0777 app/config
 chmod -Rf 0777 app/storage
@@ -11,3 +12,18 @@ gcc nginx/nginx_root.c -o nginx/nginx_root
 chown root nginx/nginx_root
 chmod u=rwx,go=xr,+s nginx/nginx_root
 echo "Nginx service created successfully!";
+
+TIME=`date +%b-%d-%y`
+mkdir /etc/nginx/conf.d
+mkdir /etc/nginx/snippets
+mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf-backup-$TIME
+cp nginx/nginx.conf /etc/nginx/nginx.conf
+cp nginx/fastcgi-php.conf /etc/nginx/snippets/fastcgi-php.conf
+cp nginx/auth.conf /etc/nginx/conf.d/auth.conf
+echo "Nginx server configuration updated"
+
+#Create Nginx /etc/init.d/nginx script
+#Assign 777 permission on /etc/init.d/nginx
+#Update service path in nginx-root.sh script
+#Create soft link of configs to /etc/nginx/conf.d/
+#Handle /auth redirection issue
