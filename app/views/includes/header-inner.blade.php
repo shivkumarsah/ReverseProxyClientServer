0c2ff265bdeb20ddcb3f2b3cf1c21204a218f730
Shiv K Sah <shiv.sah@icreon.com>
2015-10-12 18:12:19 +0530

    CSS/JS file at relative path

--- app/views/includes/header-inner.blade.php
+++ app/views/includes/header-inner.blade.php
@@ -1,8 +1,8 @@
-{{ HTML::style('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css') }}
-{{ HTML::style('http://angular-ui.github.com/ng-grid/css/ng-grid.css') }}
+{{ HTML::style('css/bootstrap.min.css') }}
+<!-- {{ HTML::style('http://angular-ui.github.com/ng-grid/css/ng-grid.css') }} -->
 {{ HTML::style('css/awesome-bootstrap-checkbox.css') }}
-{{ HTML::style('//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css') }}
-{{ HTML::style('//fonts.googleapis.com/css?family=Raleway:100,200,300,400,500,600,700,800,900') }}
+{{ HTML::style('css/font-awesome.min.css') }}
+{{ HTML::style('css/family-raleway-100-200-300-400-500-600-700-800-900.css') }}
 {{ HTML::style('css/dashboard.css') }}
 {{ HTML::style('css/ngDialog.css') }}
 {{ HTML::style('css/ngDialog-theme-default.css') }}
@@ -12,12 +12,11 @@
 <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
 <!-- Bootstrap -->
 
-
 <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 <!--[if lt IE 9]>
-{{ HTML::script('https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js') }}
-{{ HTML::script('https://oss.maxcdn.com/respond/1.4.2/respond.min.js') }}00
+{{ HTML::script('js/html5shiv.min.js') }}
+{{ HTML::script('js/respond.min.js') }}00
 
 <![endif]-->
 
